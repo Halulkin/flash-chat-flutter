0dd58be39e20f98616d06beed2150210dc99ad85
halulkin <halulkin@gmail.com>
2021-09-24 15:51:05 +0200

    Fixed problem with RenderFlex in Login and Register screens, while keyboard visible

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -20,47 +20,51 @@ class _LoginScreenState extends State<LoginScreen> {
         backgroundColor: Colors.white,
       ),
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: 'logo',
-              child: Container(
-                height: 200.0,
-                child: Image.asset('images/logo.png'),
-              ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(24.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Hero(
+                  tag: 'logo',
+                  child: Container(
+                    height: 200.0,
+                    child: Image.asset('images/logo.png'),
+                  ),
+                ),
+                SizedBox(
+                  height: 48.0,
+                ),
+                TextField(
+                    style: TextStyle(color: Colors.black),
+                    onChanged: (value) {
+                      //Do something with the user input.
+                    },
+                    decoration: kTextFieldDecoration.copyWith(
+                        hintText: "Enter your email")),
+                SizedBox(
+                  height: 8.0,
+                ),
+                TextField(
+                    style: TextStyle(color: Colors.black),
+                    onChanged: (value) {
+                      //Do something with the user input.
+                    },
+                    decoration: kTextFieldDecoration.copyWith(
+                        hintText: "Enter your password")),
+                SizedBox(
+                  height: 24.0,
+                ),
+                RoundedButton(
+                    color: Colors.lightBlueAccent,
+                    title: 'Log In',
+                    onPressed: () => () {})
+              ],
             ),
-            SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-                style: TextStyle(color: Colors.black),
-                onChanged: (value) {
-                  //Do something with the user input.
-                },
-                decoration: kTextFieldDecoration.copyWith(
-                    hintText: "Enter your email")),
-            SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-                style: TextStyle(color: Colors.black),
-                onChanged: (value) {
-                  //Do something with the user input.
-                },
-                decoration: kTextFieldDecoration.copyWith(
-                    hintText: "Enter your password")),
-            SizedBox(
-              height: 24.0,
-            ),
-            RoundedButton(
-                color: Colors.lightBlueAccent,
-                title: 'Log In',
-                onPressed: () => () {})
-          ],
+          ),
         ),
       ),
     );
--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -10,6 +10,9 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  late String email;
+  late String password;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,45 +25,55 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
       backgroundColor: Colors.white,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: 'logo',
-              child: Container(
-                height: 200.0,
-                child: Image.asset('images/logo.png'),
-              ),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Hero(
+                  tag: 'logo',
+                  child: Container(
+                    height: 200.0,
+                    child: Image.asset('images/logo.png'),
+                  ),
+                ),
+                SizedBox(
+                  height: 48.0,
+                ),
+                TextField(
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.black),
+                    onChanged: (value) {
+                      email = value;
+                    },
+                    decoration: kTextFieldDecoration.copyWith(
+                        hintText: "Enter your email")),
+                SizedBox(
+                  height: 8.0,
+                ),
+                TextField(
+                    obscureText: true,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.black),
+                    onChanged: (value) {
+                      password = value;
+                    },
+                    decoration: kTextFieldDecoration.copyWith(
+                        hintText: "Enter your password")),
+                SizedBox(
+                  height: 24.0,
+                ),
+                RoundedButton(
+                    color: Colors.blueAccent,
+                    title: 'Register',
+                    onPressed: () {
+                      print(email);
+                      print(password);
+                    }),
+              ],
             ),
-            SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-                style: TextStyle(color: Colors.black),
-                onChanged: (value) {
-                  //Do something with the user input.
-                },
-                decoration: kTextFieldDecoration.copyWith(
-                    hintText: "Enter your email")),
-            SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-                style: TextStyle(color: Colors.black),
-                onChanged: (value) {
-                  //Do something with the user input.
-                },
-                decoration: kTextFieldDecoration.copyWith(
-                    hintText: "Enter your password")),
-            SizedBox(
-              height: 24.0,
-            ),
-            RoundedButton(
-                color: Colors.blueAccent,
-                title: 'Register',
-                onPressed: () => () {}),
-          ],
+          ),
         ),
       ),
     );

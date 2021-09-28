21e837056d22cd80a866789b6974c96d7ccc20cf
Halulkin <halulkin@gmail.com>
2021-09-28 16:33:04 +0200

    feat: implemented functionality to send messages to firebase

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flash/constants.dart';
 import 'package:flutter/material.dart';
@@ -11,7 +12,11 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   final _auth = FirebaseAuth.instance;
+  final _fireStore = FirebaseFirestore.instance;
+
   late User loggedUser;
+  late String messageText;
+
 
   @override
   void initState() {
@@ -42,7 +47,6 @@ class _ChatScreenState extends State<ChatScreen> {
               icon: Icon(Icons.close),
               onPressed: () {
                 _auth.signOut();
-                //Implement logout functionality
               }),
         ],
         title: Text('⚡️Chat'),
@@ -61,14 +65,17 @@ class _ChatScreenState extends State<ChatScreen> {
                   Expanded(
                     child: TextField(
                       onChanged: (value) {
-                        //Do something with the user input.
+                        messageText = value;
                       },
                       decoration: kMessageTextFieldDecoration,
                     ),
                   ),
                   TextButton(
                     onPressed: () {
-                      //Implement send functionality.
+                      _fireStore.collection('messages').add({
+                        'text': messageText,
+                        'sender': loggedUser.email,
+                      });
                     },
                     child: Text(
                       'Send',
